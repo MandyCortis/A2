bd405dbf7fa78cbd55d09c24b8041d057f65663c
mandycortis <mandy.cortis.c10325@mcast.edu.mt>
2021-01-21 11:35:58 +0100

    test

---
+++ Scripts/StartGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    string name;
+    public void OnStartGame(string level1)
+    { 
+        SceneManager.LoadScene(level1);
+
+        //Enter where to send things here
+
+
+
+        //on start new game, gamemanager all values must be default
+
+    }
+
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+}
---
+++ Scripts/SubmitName.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubmitName : MonoBehaviour
+{
+    private string name = null;
+    public void OnValueChange()
+    {
+        InputField inputField = GameObject.Find("UsernameField").GetComponent<InputField>();
+        string name = inputField.text;
+
+        if(name.Length >= 3)
+        {
+            GameObject.Find("StartButton").GetComponent<Button>().interactable = true;
+        }
+
+        if(name.Length < 3)
+        {
+            GameObject.Find("StartButton").GetComponent<Button>().interactable = false;
+        }
+    }
+
+    public void OnEndEdit()
+    {
+        InputField inputField = GameObject.Find("UsernameField").GetComponent<InputField>();
+        name = inputField.text;
+
+        GameObject.Find("StartSceneScripts").GetComponent<StartGame>().SetName(name);
+    }
+
+    public string GetName()
+    { 
+        return name;
+    }
+}
